77a4b366a021706793eb493aff5bf024c83e1072
igortheo <150299503+igortheo@users.noreply.github.com>
2023-12-06 22:07:55 -0300

    Codificação da Autenticação de Usuário

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("Usuario: igortheo");
+        System.out.println("Senha: ######");
+
+        System.out.println("AUTENTICAÇÃO EFETUADA COM SUCESSO ");
+
     }
 }
