e527ddffb7d3c4b1196faa67c8fe80be1989cab7
Tiago Saddi Domingues <tiago.saddi@gmail.com>
2023-12-06 23:10:26 -0300

    Codificação da tarefa pagina de finalização da compra

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Digite o cartão de compra");
+        System.out.println("Compra finalizada com sucesso!");
     }
 }
