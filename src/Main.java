f70bedb83e3ceb40a2726da00ad574ee91a8e94d
igortheo <150299503+igortheo@users.noreply.github.com>
2023-12-06 21:56:11 -0300

    Codificação da tarefa Cadastro de Usuários

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("Digite seu nome de usuario:");
+        System.out.println("Usuario: igortheo");
+        System.out.println("Senha: ######");
+        System.out.println("CADASTRO FINALIZADO COM SUCESSO ");
     }
 }
